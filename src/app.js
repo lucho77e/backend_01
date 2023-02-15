dda8d3bb94adac028cd7e52defe3e67dab3b8378
Luciano Marotte <lucianomarotte@gmail.com>
2023-02-15 15:45:41 -0300

    Entrega final

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser')
 
 let productos = []
 let resultado
+let carritos =  []
 
 // Función para leer el archivo JSON ya existente y mostrar los resultados por consola
 const leerFileObjetos = async() => {
-   let resultado = await fs.promises.readFile('../objetos.json', 'utf-8')
+   let resultado = await fs.promises.readFile('../productos.json', 'utf-8')
+   let resultado2 = await fs.promises.readFile('../carritos.json', 'utf-8')
    productos = JSON.parse(resultado)
+   carritos = JSON.parse(resultado2)
 }
 leerFileObjetos()
 
@@ -28,20 +31,16 @@ app.get('/api/products', (req, res) => {
     }
 })
 
-
-
-
 app.delete('/api/products/:pid', (req, res) => {
     let idProducto = req.params.pid
     let index = productos.findIndex(x => x.id == idProducto);
 
 
     if (index != -1) {       
-        console.log(index)
 
         let eliminado = productos.splice(index, 1)
 
-        fs.writeFileSync('../objetos.json', JSON.stringify(productos))
+        fs.writeFileSync('../productos.json', JSON.stringify(productos))
         res.json(productos)
         
     } else {
@@ -75,7 +74,7 @@ app.put('/api/products/:pid', (req, res) => {
          
         if ((productoParaAgregar.title) && (productoParaAgregar.description) && (productoParaAgregar.code) && (productoParaAgregar.price) && (productoParaAgregar.stock) && (productoParaAgregar.category)) {
             productos[index] = productoParaAgregar
-            fs.writeFileSync('../objetos.json', JSON.stringify(productos))
+            fs.writeFileSync('../productos.json', JSON.stringify(productos))
             res.json(productoParaAgregar)
         } else {
             res.send("Faltan parámetros")
@@ -98,7 +97,7 @@ app.post('/api/products/', (req, res) => {
    
     if ((productoParaAgregar.title) && (productoParaAgregar.description) && (productoParaAgregar.code) && (productoParaAgregar.price) && (productoParaAgregar.code) && (productoParaAgregar.stock) && (productoParaAgregar.category)) {
         productos.push(productoParaAgregar)
-        fs.writeFileSync('../objetos.json', JSON.stringify(productos))
+        fs.writeFileSync('../productos.json', JSON.stringify(productos))
         res.json(productoParaAgregar)
     } else {
         res.send("Faltan parámetros")
@@ -107,108 +106,158 @@ app.post('/api/products/', (req, res) => {
 })
 
 
+app.post('/api/carts/', (req, res) => {
+    let nuevoCarrito = {
+        id: carritos.length,
+        products: []
+    }
+    carritos.push(nuevoCarrito)
+
+    res.send(`Nuevo carrito creado, id: ${nuevoCarrito.id}`)
+    fs.writeFileSync('../carritos.json', JSON.stringify(carritos))
+
+})
+
+app.post('/api/carts/:cid/:pid', (req, res) => {
+    let idCarrito = req.params.cid
+    let idProducto = req.params.pid
+    let index = productos.findIndex(x => x.id == idProducto);
+
+    if(carritos[idCarrito] && index != -1) {
+        
+        let indexProducto = carritos[idCarrito].products.findIndex(x => x.id == idProducto)
+
+        if (indexProducto == -1) {
+            carritos[idCarrito].products.push({
+                id: idProducto,
+                quantity: 1
+            })
+        } else {
+            carritos[idCarrito].products[indexProducto].quantity = carritos[idCarrito].products[indexProducto].quantity + 1
+        }
+
+        res.json(carritos)
+        fs.writeFileSync('../carritos.json', JSON.stringify(carritos))
+    
+    } else {
+        res.send("El carrito o el producto no existen")
+    }
+
+})
+
+
+app.get('/api/carts/:cid', (req, res) => {
+    let idCarrito = req.params.cid
+    let carritoFiltrado = carritos[idCarrito]
+    if (carritoFiltrado) {
+        res.send(carritoFiltrado.products)
+    } else {
+        res.send("El objeto no existe")
+    }
+})
+
+
+
 app.listen(8080, () => console.log("Servidor escuchando 8080"))
-
-
 
 
 // Confirmo si ya existe el archivo JSON
 let existeFile = false
-if (fs.existsSync("./objetos.json")){
+if (fs.existsSync("./productos.json")){
     existeFile = true
 }
 
-// Función para crear por primera vez el archivo JSON, con su primer objeto dentro
-const crearFileObjetos = async(param) => {
-    await fs.promises.writeFile('./objetos.json', param)
-}
+// // Función para crear por primera vez el archivo JSON, con su primer objeto dentro
+// const crearFileObjetos = async(param) => {
+//     await fs.promises.writeFile('./productos.json', param)
+// }
 
-// Función para agregar un objeto al archivo JSON ya existente
-const updateFileObjetos = async(param) => {
-    await fs.promises.appendFile('./objetos.json', param)
-}
+// // Función para agregar un objeto al archivo JSON ya existente
+// const updateFileObjetos = async(param) => {
+//     await fs.promises.appendFile('./productos.json', param)
+// }
 
 
-// Función combinada: Chequea si el JSON existe. Si existe, agrega el objeto. Si no, lo crea con el objeto.
-function addProductFileObjetos (producto) {
-    if (existeFile = false) {
-        crearFileObjetos(JSON.stringify(producto))
+// // Función combinada: Chequea si el JSON existe. Si existe, agrega el objeto. Si no, lo crea con el objeto.
+// function addProductFileObjetos (producto) {
+//     if (existeFile = false) {
+//         crearFileObjetos(JSON.stringify(producto))
         
-    } else {
-        updateFileObjetos(JSON.stringify(producto))
-        updateFileObjetos("\n")
-    }
-}
+//     } else {
+//         updateFileObjetos(JSON.stringify(producto))
+//         updateFileObjetos("\n")
+//     }
+// }
 
 
 
-class ProductManager {
-    constructor (productsColection){
-        if (existeFile) {
-            resultado = fs.readFileSync('./objetos.json', 'utf-8')
-            productos.push(JSON.parse(resultado)) 
-        } else {
-            productos = []
-        }
-        // this.products = productos
-        console.log("Nuevo objeto creado")
-    }
+// class ProductManager {
+//     constructor (productsColection){
+//         if (existeFile) {
+//             resultado = fs.readFileSync('./productos.json', 'utf-8')
+//             productos.push(JSON.parse(resultado)) 
+//         } else {
+//             productos = []
+//         }
+//         // this.products = productos
+//         console.log("Nuevo objeto creado")
+//     }
 
-    getProducts() {
-        if (existeFile) {
-            resultado = fs.readFileSync('./objetos.json', 'utf-8')
-            productos = JSON.parse(resultado)
-        }
-        console.log("El listado de productos es:")
-        console.log(productos)
-    }
+//     getProducts() {
+//         if (existeFile) {
+//             resultado = fs.readFileSync('./productos.json', 'utf-8')
+//             productos = JSON.parse(resultado)
+//         }
+//         console.log("El listado de productos es:")
+//         console.log(productos)
+//     }
     
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+//     addProduct(title, description, price, thumbnail, code, stock) {
         
-        if (stock != undefined) {
+//         if (stock != undefined) {
 
-            let filtrado = productos.filter(function(el) {
-                return el.code === code;
-            })
+//             let filtrado = productos.filter(function(el) {
+//                 return el.code === code;
+//             })
 
-            if (filtrado.length == 0) {
-                let productoParaAgregar =[{
-                    id: productos.length,
-                    title: title,
-                    description: description,
-                    price: price,
-                    thumbnail: thumbnail,
-                    code: code,
-                    stock: stock
-                }]
-                if (existeFile) {
-                    fs.appendFileSync('./objetos.json', '\n'+JSON.stringify(productoParaAgregar))
-                } else {
-                    fs.writeFileSync('./objetos.json', JSON.stringify(productoParaAgregar))
-                    existeFile = true
-                }
+//             if (filtrado.length == 0) {
+//                 let productoParaAgregar =[{
+//                     id: productos.length,
+//                     title: title,
+//                     description: description,
+//                     price: price,
+//                     thumbnail: thumbnail,
+//                     code: code,
+//                     stock: stock
+//                 }]
+//                 if (existeFile) {
+//                     fs.appendFileSync('./productos.json', '\n'+JSON.stringify(productoParaAgregar))
+//                 } else {
+//                     fs.writeFileSync('./productos.json', JSON.stringify(productoParaAgregar))
+//                     existeFile = true
+//                 }
 
 
-            } else {
-            console.log("Error: Ya existe un producto con el mismo Código")
-            }
+//             } else {
+//             console.log("Error: Ya existe un producto con el mismo Código")
+//             }
 
-        } else {
-            console.log("Error: Faltan especificar algunos campos. Debe ingresar los parámetros Título, Descripción, Precio, Imagen, Código y Stock")
-        }
-    }
+//         } else {
+//             console.log("Error: Faltan especificar algunos campos. Debe ingresar los parámetros Título, Descripción, Precio, Imagen, Código y Stock")
+//         }
+//     }
 
-    getProductsById(id) {
-        let filtrado = productos.filter(function(el) {
-            return el.id === id;
-        })
-        if (filtrado.length > 0) {
-            console.log(filtrado)
-        } else {
-            console.log("Not found")
-        }
+//     getProductsById(id) {
+//         let filtrado = productos.filter(function(el) {
+//             return el.id === id;
+//         })
+//         if (filtrado.length > 0) {
+//             console.log(filtrado)
+//         } else {
+//             console.log("Not found")
+//         }
 
-    }
-}
+//     }
+// }
 
