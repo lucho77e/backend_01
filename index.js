f1ecec7556d8900271d185a8cee74f8342eec989
lucho77e <64375337+lucho77e@users.noreply.github.com>
2023-04-26 19:26:57 +0000

    Nueva entrega desf 02 Nueva consigna

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
 const fs = require('fs')
-const { json } = require('stream/consumers')
-const { CLIENT_RENEG_LIMIT } = require('tls')
+const path = require('path')
 
 let productos = []
 let resultado
-
-// Confirmo si ya existe el archivo JSON
+let ruta 
 let existeFile = false
-if (fs.existsSync("./objetos.json")){
-    existeFile = true
-}
-
-// // Función para crear por primera vez el archivo JSON, con su primer objeto dentro
-// const crearFileObjetos = async(param) => {
-//     await fs.promises.writeFile('./objetos.json', param)
-// }
-
-// // Función para agregar un objeto al archivo JSON ya existente
-// const updateFileObjetos = async(param) => {
-//     await fs.promises.appendFile('./objetos.json', param)
-// }
-
-// // Función para leer el archivo JSON ya existente y mostrar los resultados por consola
-// const leerFileObjetos = () => {
-//    let resultado = fs.promises.readFileSync('./objetos.json', 'UTF-8')
-//    productos = JSON.parse(resultado)
-//    console.log("El listado de productos es:")
-//    console.log(productos)
-// }
-
-// // Función combinada: Chequea si el JSON existe. Si existe, agrega el objeto. Si no, lo crea con el objeto.
-// function addProductFileObjetos (producto) {
-//     if (existeFile = false) {
-//         crearFileObjetos(JSON.stringify(producto,null,2))
-        
-//     } else {
-//         updateFileObjetos(JSON.stringify(producto,null,2))
-//         updateFileObjetos("\n")
-//     }
-// }
-
 
 
 class ProductManager {
-    constructor (){
+    constructor (path){
+        ruta = path
+
+        // Confirmo si ya existe el archivo JSON
+        if (fs.existsSync(ruta)){
+            existeFile = true
+        }
+
         if (existeFile) {
-            resultado = JSON.parse(fs.readFileSync('./objetos.json', 'UTF-8')) 
+            resultado = JSON.parse(fs.readFileSync(ruta, 'UTF-8')) 
             productos = resultado 
         } else {
             productos = []
@@ -55,7 +27,7 @@ class ProductManager {
 
     getProducts() {
         if (existeFile) {
-            resultado =  JSON.parse(fs.readFileSync('./objetos.json', 'UTF-8'))
+            resultado =  JSON.parse(fs.readFileSync(ruta, 'UTF-8'))
             productos = resultado
         }
         console.log("El listado de productos es:")
@@ -72,7 +44,13 @@ class ProductManager {
             })
 
             if (filtrado.length == 0) {
-                let next_id = productos[productos.length-1].id+1
+                let next_id
+                if (productos.length > 0) {
+                    next_id = productos[productos.length-1].id+1
+                } else {
+                    next_id = 1
+                }
+
                 let productoParaAgregar ={
                     id: next_id,
                     title: title,
@@ -84,7 +62,7 @@ class ProductManager {
                 }
                 productos.push(productoParaAgregar)
                 let data_json = JSON.stringify(productos,null,2)
-                await fs.promises.writeFile('./objetos.json', data_json)
+                await fs.promises.writeFile(ruta, data_json)
                 existeFile = true
             } else {
             console.log("Error: Ya existe un producto con el mismo Código")
@@ -117,7 +95,7 @@ class ProductManager {
 
         productos[indice] = productoParaModificar
         let data_json = JSON.stringify(productos,null,2)
-        await fs.promises.writeFile('./objetos.json', data_json)
+        await fs.promises.writeFile(ruta, data_json)
 
     }
 
@@ -125,55 +103,50 @@ class ProductManager {
     async deleteProduct(id) {
         productos = productos.filter(el => el.id!==id)
         let data_json = JSON.stringify(productos,null,2)
-        await fs.promises.writeFile('./objetos.json', data_json)
+        await fs.promises.writeFile(ruta, data_json)
     }
 }
 
-//
+
+
 
 async function manager() {
-    let instancia = new ProductManager();
-    instancia.updateProduct(55, {
-        price: 110
-      })
-    // await instancia.addProduct("Manaos", "Envase retornable 1l", 60, "Sin imagen", "zzz", 25)
-    // await instancia.deleteProduct(1)
+        console.log("*** Se creará una instancia de la clase “ProductManager”")
+        let instancia = new ProductManager('./objetos.json');
+        console.log(" ")
+
+        console.log("*** Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []")
+        await instancia.getProducts()
+        console.log(" ")
+
+        console.log("*** Se llamará al método “addProduct” con los campos: title: “producto prueba” description:”Este es un producto prueba” price:200, thumbnail:”Sin imagen” code:”abc123”, stock:25")
+        await instancia.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+        console.log("El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE")
+        console.log(" ")
+
+        console.log("*** Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado")
+        await instancia.getProducts()
+        console.log(" ")
+
+        console.log("*** Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.")
+        await instancia.getProductsById(1)
+        console.log(" ")
+
+        await instancia.getProductsById(30)
+        console.log(" ")
+
+        console.log("*** Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.")
+        await instancia.updateProduct(1, {
+                     price: 970
+                   })
+        console.log(" ")
+
+        console.log("*** Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.")
+        await instancia.deleteProduct(1)
+        console.log(" ")
 }
 
 manager()
-
-/*
-
-console.log("*** Se creará una instancia de la clase “ProductManager”")
-let instancia = new ProductManager();
-console.log(" ")
-
-console.log("*** Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []")
-instancia.getProducts()
-console.log(" ")
-
-console.log("*** Se llamará al método “addProduct” con los campos: title: “producto prueba” description:”Este es un producto prueba” price:200, thumbnail:”Sin imagen” code:”abc123”, stock:25")
-instancia.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
-console.log("El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE")
-console.log(" ")
-
-console.log("*** Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado")
-instancia.getProducts()
-console.log(" ")
-
-console.log("*** Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.")
-instancia.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
-console.log(" ")
-
-console.log("*** Se evaluará que getProductById devuelva error si no encuentra el producto")
-instancia.getProductsById(50)
-console.log(" ")
-
-console.log("*** Se evaluará que getProductById devuelva el producto en caso de encontrarlo")
-instancia.getProductsById(0)
-console.log(" ")
-
-*/
 
 
 
